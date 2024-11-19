6761cc9b7968acac20b9fcd0a0b532f273c7d06b
Jack <JVM406@student.bham.ac.uk>
2024-11-19 16:41:08 +0000

    All whole-file operations completed and working

--- Q4.c
+++ Q4.c
@@ -5,7 +5,7 @@
 //(Fix so that it doesn't bug out if you enter more than 100 chars)
 int getFileNameInput(char *fileNameInput){
     //Get file name input    
-    printf("Input file name to create (without .txt): ");
+    printf("Input file name (without .txt): ");
     fgets(fileNameInput, 100, stdin); 
     
     //The fgets functions includes an enter (\n) so need to remove this
@@ -21,6 +21,7 @@ int getFileNameInput(char *fileNameInput){
         fileNameInput[j] = fileNameInput[j];
     }
     fileNameInput[fileNameInputLen] = '\0';
+    //The input is formatted as a string
     return 0;
 }
 
@@ -38,10 +39,12 @@ int fileExists(char *fileName){
 
 int createFile(){
     
+    //Get file name input
+    printf("Creating a file from inputted file name...\n");
     char fileNameInput[100];
     getFileNameInput(fileNameInput);
 
-    //Format file name
+    //Format file name string
     char *fileName = malloc(strlen("./") + strlen(fileNameInput) + strlen(".txt") + 1);
     strcpy(fileName, "./");
     strcat(fileName, fileNameInput);
@@ -50,12 +53,14 @@ int createFile(){
     //Check if file exists
     if (fileExists(fileName)){
         printf("File already exists!\n");
+        free(fileName);
         return -1;
     }
-    //Else...
+    
     FILE *f1 = fopen(fileName, "w");
     if (f1 == NULL){
         printf("Error with creating this file\n");
+        free(fileName);
         return -1;
     }
     printf("File successfully created!\n");
@@ -65,34 +70,145 @@ int createFile(){
 }
 
 int copyFile(){
-    /*char *fileInput = argv[1];
-        
-    char *fileName = malloc(strlen("./") + strlen(fileInput) + 1);
-    strcpy(fileName, "./");
-    strcat(fileName, fileInput);
+    
+    //Get to and from file name input
+    printf("File name to copy FROM\n");
+    char fileNameInputFROM[100];
+    getFileNameInput(fileNameInputFROM);
+    
+    printf("File name to copy TO\n");
+    char fileNameInputTO[100];
+    getFileNameInput(fileNameInputTO);
 
     
+    //Format file name strings
+    char *fileNameFROM = malloc(strlen("./") + strlen(fileNameInputFROM) + strlen(".txt") + 1);
+    strcpy(fileNameFROM, "./");
+    strcat(fileNameFROM, fileNameInputFROM);
+    strcat(fileNameFROM, ".txt");
+
+    char *fileNameTO = malloc(strlen("./") + strlen(fileNameInputTO) + strlen(".txt") + 1);
+    strcpy(fileNameTO, "./");
+    strcat(fileNameTO, fileNameInputTO);
+    strcat(fileNameTO, ".txt");
     
-    FILE *fp1 = fopen(fileName,"r"); //may need rb and wb
-    FILE *fp2 = fopen("./file2.txt", "w");
+    //Check if file exists
+    if (fileExists(fileNameTO)){
+        printf("File already exists!\n");
+        printf("You must copy to a new file!\n");
+        free(fileNameFROM);
+        free(fileNameTO);
+        return -1;
+    }
+            
+    FILE *fFROM = fopen(fileNameFROM,"r"); //may need rb and wb    
+    FILE *fTO = fopen(fileNameTO, "w");
+
+    if (fFROM == NULL){
+        printf("Error with opening this file\n");
+        free(fileNameFROM);
+        free(fileNameTO);
+        return -1;
+    }
+
+    if (fTO == NULL){
+        printf("Error with creating this file\n");
+        free(fileNameFROM);
+        free(fileNameTO);
+        return -1;
+    }
     
-    int c;    
-    c = fgetc(fp1);
-    while(c!=EOF){
-        //fprintf(fp2, "%c", c);
-        fputc(c, fp2);
-        c=fgetc(fp1);
-    }    
-    
-    fclose(fp1);
-    fclose(fp2);
-    free(fileName);*/
+    //Go through and copy file character by character
+    int c;
+    c = fgetc(fFROM);
+    while(c!=EOF){        
+        fputc(c, fTO);
+        c=fgetc(fFROM);
+    }
+
+    printf("File successfully copied!\n");
+    fclose(fFROM);
+    fclose(fTO);
+    free(fileNameFROM);
+    free(fileNameTO);    
     return 0;
 }
 
-int deleteFile(){return 0;}
+int deleteFile(){
+    //Get file name input
+    printf("Deleting a file from inputted file name...\n");
+    char fileNameInput[100];
+    getFileNameInput(fileNameInput);
 
-int showFile(){return 0;}
+    //Format file name string
+    char *fileName = malloc(strlen("./") + strlen(fileNameInput) + strlen(".txt") + 1);
+    strcpy(fileName, "./");
+    strcat(fileName, fileNameInput);
+    strcat(fileName, ".txt");
+        
+    //Check if file exists
+    if (!fileExists(fileName)){
+        printf("File doesn't exist!\n");
+        free(fileName);
+        return -1;
+    }
+    
+    if (remove(fileName) == 0){
+        printf("File successfully deleted!\n");
+        free(fileName);
+        return 0;
+    }
+    else {
+        printf("Error with deleting this file\n");
+        free(fileName);
+        return -1;
+    }
+}
+
+int showFile(){
+    //Get file name input
+    printf("Displaying contents from inputted file name...\n");
+    char fileNameInput[100];
+    getFileNameInput(fileNameInput);
+
+    //Format file name string
+    char *fileName = malloc(strlen("./") + strlen(fileNameInput) + strlen(".txt") + 1);
+    strcpy(fileName, "./");
+    strcat(fileName, fileNameInput);
+    strcat(fileName, ".txt");
+        
+    //Check if file exists
+    if (!fileExists(fileName)){
+        printf("File doesn't exist!\n");
+        free(fileName);
+        return -1;
+    }
+    
+    //Open file
+    FILE *f1 = fopen(fileName, "r");
+    if (f1 == NULL){
+        printf("Error with opening this file\n");
+        free(fileName);
+        return -1;
+    }
+    
+    //Go through and print file character by character
+    printf("\nHere is the file:\n");
+    printf("--------------------------------\n");
+    int c;
+    c = fgetc(f1);
+    while(c!=EOF){        
+        printf("%c", c);
+        c=fgetc(f1);
+    }
+    printf("\n");
+    printf("--------------------------------\n");    
+    
+    printf("File successfully displayed!\n");
+    fclose(f1);
+    free(fileName);
+    return 0;
+}
 
 //Line operations
 
@@ -114,6 +230,7 @@ int showNumLines(){return 0;}
 //Word count, Char count
 //Rename file
 //Write to a file
+//Show current text files in dir
 //Something cooler?
 
 int options(){
@@ -138,7 +255,7 @@ int options(){
 
     printf("11- ?\n");
     printf("12- ?\n");
-    printf("\n");}
+    }
 
 int main(){
     printf("---GROUP 127 TEXT EDITOR---\n");
@@ -149,7 +266,7 @@ int main(){
     
     while (!finished){
         
-        printf("Choose option--> ");
+        printf("\nChoose option--> ");
         char term;
         //scanf returns the number of successfully read items
         //a valid input must be an integer followed by an enter (\n)
@@ -176,8 +293,8 @@ int main(){
                 deleteFile();
                 break;
             case 4:
-                break;
                 showFile();
+                break;                
             case 5:
                 appendLine();
                 break;
